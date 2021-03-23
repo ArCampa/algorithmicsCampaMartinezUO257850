f702371e50e0c87385b83f5d960e868a24159814
Armando <uo257850@uniovi.es>
2021-03-23 23:03:03 +0100

    Update Main.java

--- src/main/java/algstudent/s4/Main.java
+++ src/main/java/algstudent/s4/Main.java
@@ -1,9 +1,11 @@
 package algstudent.s4;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String arg[]) {
+    public static void main(String arg[]) throws IOException {
         new SegmentsPlacement("src/main/java/algstudent./s4/game1.txt");
         new SegmentsPlacement("src/main/java/algstudent./s4/game2.txt");
-        new SegmentsPlacementTimes();
+        new SegmentsPlacementTimes().simulate();;
     }
 }
